46f91f6cf8367e7aa63df03361e108bd838f54ab
Leon Nicolhas <leonnhsalas@gmail.com>
2023-10-10 15:03:10 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+// console.log("helloworld");
+
+// var numeroUno = 37;
+
+// console.log(numeroUno)
+
+// console.log("Este es: " + numeroUno)
+
+// var numeroDos = 38
+
+// /* console.log("El resultado de la suma" + numeroUno + numeroDos)
+
+// console.log("El resultado de la resta" + numeroUno - numeroDos)
+
+// console.log("El resultado de la multipicación" + numeroUno * numeroDos)
+
+// console.log("El resultado de la division" + numeroUno / numeroDos) */
+
+// if(numeroUno > numeroDos){
+//     console.log("Este es el numero mayor:")
+// }else{
+//     console.log("Este es el numero menor:")
+// }
+
+
+
+// var mayordeEdad = 18
+
+// var edadActual = 64
+
+// var MenordeEdad = 17
+
+// var AdultoMayor = 65
+
+// if(edadActual > mayordeEdad && edadActual < AdultoMayor || edadActual == mayordeEdad){
+//     console.log("Ya eres un adulto:")
+// }else if(edadActual < mayordeEdad){
+//     console.log("Todavía no eres un adulto")
+// }else{
+//     console.log("Ya eres un adulto mayor")
+// }
+
+
+// Para mañana Consulta cuales son Estructuras repetitivas en Javascript
+
+//Para el viernes Lo mismo de la clase  Pero agregar Adulto Mayor en 65
+
+//  var numeroUno = 15;
+
+//  var limite1 = 30;
+
+//  var limite2 = 5
+
+//  while(numeroUno <= limite1 && numeroUno >= limite2){
+
+//      console.log(numeroUno)
+
+//      numeroUno++
+//  }
+var numero = 1000000000000000
+
+var contador = 1
+
+var resultado = numero * contador
+
+var limite = 10
+
+while(contador <= limite){
+
+    resultado = numero * contador
+
+    console.log(numero + "x" + contador + "=" + resultado)
+
+    contador++
+}
+
+
